6d6876131a7d0f2d7aa3ad4513bad6b8a5fa2147
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-02-29 18:25:59 -0300

    add router-outlet, add rotas listar-pensamento, criar-pensamento

--- memoteca-com-angular-14/memoteca/src/app/app-routing.module.ts
+++ memoteca-com-angular-14/memoteca/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar-pensamento',
+    pathMatch: 'full',
+  },
+  {
+    path: 'criar-pensamento',
+    component: CriarPensamentoComponent
+  },
+  {
+    path: 'listar-pensamento',
+    component: ListarPensamentoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
