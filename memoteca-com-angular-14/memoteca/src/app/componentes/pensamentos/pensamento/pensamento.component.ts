5855b9034a63e0272f1dd686c6acc954642f6e82
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-03-01 12:27:52 -0300

    add component pensamento,

---
+++ memoteca-com-angular-14/memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Nay',
+    modelo: 'modelo3',
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
