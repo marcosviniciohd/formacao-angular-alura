16de30a28b410a76fb51d5de97ecc77749f3f1db
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-03-01 15:52:40 -0300

    add *ngIf e ng-template

--- memoteca-com-angular-14/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca-com-angular-14/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [];
+  listaPensamentos = [
+    // {
+    //   conteudo: 'Passo informações para o componente filho',
+    //   autoria: 'Componente Pai',
+    //   modelo: 'modelo3',
+    // },
+    // {
+    //   conteudo: 'Minha propriedade é decorada com com @Input()',
+    //   autoria: 'Componente filho',
+    //   modelo: 'modelo2',
+    // },
+    // {
+    //   conteudo: 'Aprendendo a utilizar o @Input(), com [] property binding',
+    //   autoria: 'Componente filho',
+    //   modelo: 'modelo3',
+    // }
+  ];
 
   constructor() { }
 
